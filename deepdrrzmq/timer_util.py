f6bd568974646213f556fa21b79893617bdb0129
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2023-02-11 12:04:51 -0500

    Adjust fps args

--- deepdrrzmq/timer_util.py
+++ deepdrrzmq/timer_util.py
@@ -2,15 +2,16 @@ import time
 import collections
 
 class FPS:
-    def __init__(self,printevery=1):
+    def __init__(self,print_every_seconds=1):
         self.starttime = time.time()
-        self.printevery = printevery
+        self.printevery = print_every_seconds
         self.calls = 0
     def __call__(self):
         self.calls += 1
-        if self.calls % self.printevery == 0:
-            ret = self.printevery/(time.time()-self.starttime)
+        if time.time()-self.starttime > self.printevery:
+            ret = self.calls/(time.time()-self.starttime) 
             self.starttime = time.time()
+            self.calls = 0
             return ret
         return None
             
