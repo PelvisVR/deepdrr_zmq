71564fcec27750fe1ef4e4a412536d7a122ac316
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2023-03-24 17:04:57 -0700

    Re-enable cache

--- deepdrrzmq/utils/drr_util.py
+++ deepdrrzmq/utils/drr_util.py
@@ -6,11 +6,11 @@ joblib_cache = deepdrr.utils.data_utils.deepdrr_data_dir()/"joblib_cache"
 joblib_cache.mkdir(parents=True, exist_ok=True)
 memory = Memory(joblib_cache, verbose=9999)
 
-# @memory.cache
+@memory.cache
 def from_nifti_cached(*args, **kwargs):
     return deepdrr.Volume.from_nifti(*args, **kwargs)
 
-# @memory.cache
+@memory.cache
 def from_meshes_cached(*args, **kwargs):
     return from_meshes(*args, **kwargs)
 
