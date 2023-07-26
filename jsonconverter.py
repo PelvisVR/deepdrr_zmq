7ff5c1b0784e1b43ce18e6042da4548e442d80d1
Han Zhang <115187315+HanZhang206@users.noreply.github.com>
2023-07-26 01:20:37 -0400

    updatejsonconverter

--- jsonconverter.py
+++ jsonconverter.py
@@ -18,72 +18,77 @@ def extract_topic_data_from_log(log_file,log_folder_path):
         file_name = os.path.splitext(log_file.name)[0] 
         file_number = file_name.split("--")[-1] 
         # print(file_number + ' file '+str(len(topic_data))+' '+topic)#for debug
-        if topic not in unique_topics:  # Add unique topics to the list
-            unique_topics.append(topic)
-
-        if topic.startswith("/mp/transform/"):
-            with messages.SyncedTransformUpdate.from_bytes(entry.data) as transform:
-                # transform_dict = {}
-                msgdict['timestamp'] = transform.timestamp
-                msgdict['clientId'] = transform.clientId
-                transforms = []
-                for i in range(len(transform.transforms)):
-                    transforms.append([x for x in transform.transforms[i].data])
-                msgdict['transforms'] = transforms
-                # msgdict['transforms'] = transform_dict 
-        if topic.startswith("/mp/time/"):
-            with messages.Time.from_bytes(entry.data) as time:
-                msgdict['time'] = time.millis 
-
-        if topic.startswith("project_request/"):
-            with messages.ProjectRequest.from_bytes(entry.data) as request:
-                msgdict['requestId'] = request.requestId
-                msgdict['projectorId'] = request.projectorId
-                cameraProjections_dict_ = []
-                for i in range(len(request.cameraProjections)):
-                    current_cameraProjections = request.cameraProjections[i]
-                    cameraProjections_dict = {}
-                    camerainstrinsic_dict = {}
-                    camerainstrinsic_dict['sensorHeight'] = current_cameraProjections.intrinsic.sensorHeight
-                    camerainstrinsic_dict['sensorWidth'] = current_cameraProjections.intrinsic.sensorWidth
-                    camerainstrinsic_dict['pixelSize'] = current_cameraProjections.intrinsic.pixelSize
-                    camerainstrinsic_dict['sourceToDetectorDistance'] = current_cameraProjections.intrinsic.sourceToDetectorDistance
-                    cameraProjections_dict['intrinsic'] = camerainstrinsic_dict
-                    cameraProjections_dict['extrinsic'] = list(request.cameraProjections[i].extrinsic.data)   
-                    cameraProjections_dict_.append(cameraProjections_dict)
-                msgdict['cameraProjections'] = cameraProjections_dict_ 
-                transforms = []
-                for i in range(len(request.volumesWorldFromAnatomical)):
-                    transforms.append([x for x in request.volumesWorldFromAnatomical[i].data])
-                msgdict['volumesWorldFromAnatomical'] = transforms 
-        if topic.startswith("/project_response/"):
-        #     decode  the jpeg image from the bytes
-            image = Image.open(BytesIO(entry.data))
-            image_filename = str(image_idx) + ".jpg"
-            image_path = os.path.join(log_folder_path, image_filename)
-            image.save(image_path)
-            image_idx += 1
-
+        # if topic not in unique_topics:  # Add unique topics to the list
+        #     unique_topics.append(topic)
+        # if topic.startswith("/mp/transform/"):
+        #     with messages.SyncedTransformUpdate.from_bytes(entry.data) as transform:
+        #         # transform_dict = {}
+        #         msgdict['timestamp'] = transform.timestamp
+        #         msgdict['clientId'] = transform.clientId
+        #         transforms = []
+        #         for i in range(len(transform.transforms)):
+        #             transforms.append([x for x in transform.transforms[i].data])
+        #         msgdict['transforms'] = transforms
+        #         # msgdict['transforms'] = transform_dict 
+        # if topic.startswith("/mp/time/"):
+        #     with messages.Time.from_bytes(entry.data) as time:
+        #         msgdict['time'] = time.millis 
+        # if topic.startswith("project_request/"):
+        #     with messages.ProjectRequest.from_bytes(entry.data) as request:
+        #         msgdict['requestId'] = request.requestId
+        #         msgdict['projectorId'] = request.projectorId
+        #         cameraProjections_dict_ = []
+        #         for i in range(len(request.cameraProjections)):
+        #             current_cameraProjections = request.cameraProjections[i]
+        #             cameraProjections_dict = {}
+        #             camerainstrinsic_dict = {}
+        #             camerainstrinsic_dict['sensorHeight'] = current_cameraProjections.intrinsic.sensorHeight
+        #             camerainstrinsic_dict['sensorWidth'] = current_cameraProjections.intrinsic.sensorWidth
+        #             camerainstrinsic_dict['pixelSize'] = current_cameraProjections.intrinsic.pixelSize
+        #             camerainstrinsic_dict['sourceToDetectorDistance'] = current_cameraProjections.intrinsic.sourceToDetectorDistance
+        #             cameraProjections_dict['intrinsic'] = camerainstrinsic_dict
+        #             cameraProjections_dict['extrinsic'] = list(request.cameraProjections[i].extrinsic.data)   
+        #             cameraProjections_dict_.append(cameraProjections_dict)
+        #         msgdict['cameraProjections'] = cameraProjections_dict_ 
+        #         transforms = []
+        #         for i in range(len(request.volumesWorldFromAnatomical)):
+        #             transforms.append([x for x in request.volumesWorldFromAnatomical[i].data])
+        #         msgdict['volumesWorldFromAnatomical'] = transforms 
+        # if topic.startswith("/project_response/"):
+        # #     decode  the jpeg image from the bytes
+        #     image = Image.open(BytesIO(entry.data))
+        #     image_filename = str(image_idx) + ".jpg"
+        #     image_path = os.path.join(log_folder_path, image_filename)
+        #     image.save(image_path)
+        #     image_idx += 1
         if topic.startswith("/mp/setting"):
             with messages.SycnedSetting.from_bytes(entry.data) as setting_data:
                 setting_data_dict = {}
                 msgdict['timestamp'] = setting_data.timestamp
                 msgdict['clientId'] = setting_data.clientId
                 which = setting_data.setting.which()
-                if which == 'uiControl':
-                    setting = setting_data.setting.uiControl                   
-                    setting_data_dict['patientMaterial'] = setting.patientMaterial
-                    setting_data_dict['annotationSelection'] = list(setting.annotationError)
-                    setting_data_dict['corridorIndicator'] = setting.corridorIndicator
-                    setting_data_dict['carmIndicator'] = setting.carmIndicator
-                    setting_data_dict['webcorridorerrorselect'] = setting.webcorridorerrorselect
-                    setting_data_dict['webcorridorselection'] = setting.webcorridorselection
-                    setting_data_dict['flippatient'] = setting.flippatient
-                    setting_data_dict['viewIndicatorselfselect'] = setting.viewIndicatorselfselect
-                    msgdict['uiControl'] = setting_data_dict   
-                if which == 'arm':     
-                    setting = setting_data.setting.arm.liveCapture  
-                    msgdict['liveCapture'] = setting          
+                # if which == 'uiControl':
+                #     setting = setting_data.setting.uiControl                   
+                #     setting_data_dict['patientMaterial'] = setting.patientMaterial
+                #     setting_data_dict['annotationSelection'] = list(setting.annotationError)
+                #     setting_data_dict['corridorIndicator'] = setting.corridorIndicator
+                #     setting_data_dict['carmIndicator'] = setting.carmIndicator
+                #     setting_data_dict['webcorridorerrorselect'] = setting.webcorridorerrorselect
+                #     setting_data_dict['webcorridorselection'] = setting.webcorridorselection
+                #     setting_data_dict['flippatient'] = setting.flippatient
+                #     setting_data_dict['viewIndicatorselfselect'] = setting.viewIndicatorselfselect
+                #     msgdict['uiControl'] = setting_data_dict   
+                # if which == 'arm':     
+                #     setting = setting_data.setting.arm.liveCapture  
+                #     msgdict['liveCapture'] = setting 
+                if which == 'testrecorddata':
+                    setting = setting_data.setting.testrecorddata
+                    setting_data_dict['xraycount'] = setting.xraycount
+                    setting_data_dict['totaltime'] = setting.totaltime
+                    setting_data_dict['totaltestcount'] = setting.totaltestcount
+                    msgdict['testrecorddata'] = setting_data_dict   
+                    
+                             
         topic_data.append(msgdict)
     return topic_data, unique_topics
 def convert_pvrlog_to_json(log_folder):
